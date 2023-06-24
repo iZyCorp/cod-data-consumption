7dfed0a6ff5d104c697459885bb9fb658a0412ff
izyer <izyeeclipse@gmail.com>
2023-06-24 22:32:08 +0200

    Connect to database using yaml config file info

--- src/main/java/fr/izy/Main.java
+++ src/main/java/fr/izy/Main.java
@@ -1,25 +1,30 @@
 package fr.izy;
 
 import fr.izy.database.Database;
-import fr.izy.database.dao.OpusDAO;
-import fr.izy.database.dao.PlatformDAO;
 import fr.izy.database.dao.StatsDAO;
 import fr.izy.moon.ModernWarfare;
+import fr.izy.moon.MyListener;
 import io.github.izycorp.moonapi.components.*;
 import io.github.izycorp.moonapi.query.RequestManager;
 import org.json.JSONObject;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
+import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
+
+    private File configFile;
 
     // Database properties
     private StatsDAO statsDAO;
@@ -55,6 +60,22 @@ public class Main {
     public Main() throws Exception {
         boolean isOkay = false;
 
+        System.out.println("[!] Initializing config file...");
+        try {
+            configFile = new File("config.yml");
+            if(!configFile.exists()) {
+                File ressourceFile = new File(getClass().getClassLoader().getResource("config.yml").toURI());
+                Files.copy(ressourceFile.toPath(), configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("[!] Config file was not found, creating a new one...");
+                System.out.println("[!] Please edit the config file and restart the program.");
+                System.exit(0);
+            }
+        } catch (Exception e) {
+            System.out.println("[!] An error occurred while creating or checking the config file.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
         System.out.println("[!] Connecting to database...");
         initializeDatabase();
         System.out.println("[!] Database initialized");
@@ -63,6 +84,22 @@ public class Main {
         System.out.println("[!] Starting...");
         try {
             Scanner scanner = new Scanner(System.in);
+
+            System.out.println("[?] Would you like to TRUNCATE stats table (Remove all existing data) ? (Y/N)");
+            while(!isOkay) {
+                String input = scanner.nextLine();
+                if(input.equalsIgnoreCase("y")) {
+                    statsDAO.truncate();
+                    isOkay = true;
+                } else if(input.equalsIgnoreCase("n")) {
+                    isOkay = true;
+                } else {
+                    System.out.println("[!] Invalid input. Please enter 'Y' or 'N'.");
+                }
+            }
+
+            isOkay = false;
+
             System.out.println("[?] Enter :\n| '1' = BO3 \n| '2' = INFINITE_WARFARE \n| '3' = WWII \n| '4' = 'BO4' \n| '5' = 'MW2019' \n| '6' = ColdWar \n| '7' = Vanguard \n| '8' = MW2");
             while (!isOkay) {
                 int opus = scanner.nextInt();
@@ -125,7 +162,7 @@ public class Main {
                         break;
                 }
                 if (Arrays.asList(targetedOpus.getCompatiblePlatforms()).contains(targetedPlatform)) {
-                    //fetchedPagesAmount = dataDAO.getLatestPage(input) == 0 ? 1 : dataDAO.getLatestPage(input);
+                    fetchedPagesAmount = statsDAO.getNumberOfFetchedPage(targetedOpus, targetedPlatform);
                     isOkay = true;
                 } else System.out.println("[!] Invalid platform for this opus, please enter a valid one.");
             }
@@ -157,8 +194,13 @@ public class Main {
 
     private void initializeDatabase() {
         try {
+
+            // Reading our config file
+            Map<?, ?> config;
+            config = new Yaml().loadAs(Files.newInputStream(configFile.toPath()), Map.class);
+
             // We init our database connection
-            Database database = new Database("localhost", 5432, "moon", "postgres", "postgres");
+            Database database = new Database(config.get("host").toString(), Integer.parseInt(config.get("port").toString()), config.get("database").toString(), config.get("user").toString(), config.get("password").toString());
             System.out.println("[DATABASE] Successfully connected to database.");
 
             System.out.println("[DATABASE] Executing SQL files...");
@@ -186,7 +228,7 @@ public class Main {
         AtomicReference<Double> percentage = new AtomicReference<>((double) 0);
 
         // For loop to get our 50 000 000 accounts KDA 2500000 - 5000000 for 1 000 000 accounts
-        for (int pageIndex = fetchedPagesAmount; pageIndex < nbPageToFetch; pageIndex++) {
+        for (int pageIndex = fetchedPagesAmount; pageIndex < nbPageToFetch + 1; pageIndex++) {
 
             // We acquire a semaphore to avoid spamming the api
             threadSemaphore.acquire();
@@ -245,16 +287,27 @@ public class Main {
                     throw new RuntimeException(e);
                 }
 
+                if (percentage.get() == 100.0) {
+                    System.out.println("Finished " + targetPlatform.name());
+                    System.out.println("_____________________________________________________________");
+                    // marge d'erreur
+                    int fetchedUser = 0;
+                    try {
+                        fetchedUser = statsDAO.getNumberOfFetchedUser(targetedOpus, targetPlatform);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Total User: " + fetchedUser);
+                    System.out.println("Total Page successfully fetched: " + (fetchedUser / 20));
+                    System.out.println("Total Page failed: " + (nbPageToFetch - (fetchedUser / 20)));
+
+                }
+
                 // Interrupting the thread
                 threadSemaphore.release();
             });
 
             currentThread.start();
-
-            if (percentage.get() == 100.0) {
-                System.out.println("Finished " + targetPlatform.name());
-                System.out.println("_____________________________________________________________");
-            }
         }
     }
 }
